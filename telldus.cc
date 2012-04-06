ee94baef698cfc37bc2840ba61953e808e8ff840
Marcus <marcus@solidkod.nu>
2012-04-06 18:21:58 +0200

    add suported metods to array

--- telldus.cc
+++ telldus.cc
@@ -18,15 +18,23 @@ using namespace node;
 #define DEGREE "°"
 #endif
 
-const int SUPPORTED_METHODS =
-    TELLSTICK_TURNON |
-    TELLSTICK_TURNOFF |
-    TELLSTICK_BELL |
-    TELLSTICK_DIM;
 
-const int DATA_LENGTH = 20;
 
 namespace telldus_v8 {
+
+    const int SUPPORTED_METHODS =
+    TELLSTICK_TURNON    
+    | TELLSTICK_TURNOFF   
+    | TELLSTICK_BELL      
+    | TELLSTICK_TOGGLE    
+    | TELLSTICK_DIM       
+    | TELLSTICK_LEARN     
+    | TELLSTICK_EXECUTE   
+    | TELLSTICK_UP        
+    | TELLSTICK_DOWN      
+    | TELLSTICK_STOP;
+
+    const int DATA_LENGTH = 20;
 
     Handle<Value> TdGetNumberOfDevices( const Arguments& args ) {
         HandleScope scope;
@@ -35,6 +43,67 @@ namespace telldus_v8 {
         return scope.Close(num);
     }
 
+    Local<Object> GetSuportedMetods(int id){
+    
+        int methods = tdMethods( id, SUPPORTED_METHODS );
+        Local<Array> methodsObj = Array::New();
+        int i = 0;
+        if (methods & TELLSTICK_TURNON)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_TURNON"));
+        }
+        if (methods & TELLSTICK_TURNOFF)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_TURNOFF"));
+        }
+        if (methods & TELLSTICK_BELL)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_BELL"));
+        }
+        if (methods & TELLSTICK_TOGGLE)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_TOGGLE"));
+        }
+        if (methods & TELLSTICK_DIM)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_DIM"));
+        }
+        if (methods & TELLSTICK_UP)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_UP"));
+        }
+        if (methods & TELLSTICK_DOWN)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_DOWN"));
+        }
+        if (methods & TELLSTICK_STOP)
+        {
+            methodsObj->Set(i++, String::New("TELLSTICK_STOP"));
+        }
+
+        return methodsObj;
+
+    }
+    
+    Local<Object> GetDevice(int index){
+
+        int id = tdGetDeviceId( index );
+        char *name = tdGetName( id );
+        char *model = tdGetModel(id);
+        int methods = tdMethods( id, SUPPORTED_METHODS );
+
+        Local<Object> obj = Object::New();
+        obj->Set(String::NewSymbol("name"), String::New(name, strlen(name)));
+        obj->Set(String::NewSymbol("id"), Number::New(id));
+        obj->Set(String::NewSymbol("metods"), GetSuportedMetods(id));
+        obj->Set(String::NewSymbol("model"), String::New(model, strlen(model)));
+        
+        tdReleaseString(name);
+        tdReleaseString(model);
+
+        return obj;
+    }
+    
     Handle<Value> TdGetDevices( const Arguments& args ) {
         HandleScope scope;
         tdInit();
@@ -44,14 +113,7 @@ namespace telldus_v8 {
         Local<Array> devices = Array::New(intNumberOfDevices);
 
         for (int i = 0; i < intNumberOfDevices; i++) {
-           int id = tdGetDeviceId( i );
-           char *name = tdGetName( id );
-           int length = strlen(name);
-           Local<Object> obj = Object::New();
-           obj->Set(String::NewSymbol("name"), String::New(name, length));
-           obj->Set(String::NewSymbol("id"), Number::New(id));
-           devices->Set(i, obj);
-           tdReleaseString(name);
+           devices->Set(i, GetDevice(i));
          }
         
         return scope.Close(devices);
