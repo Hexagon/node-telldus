a74bd6b2e29ffe810da716d2a087466751aab93b
plastbox <plastbox@hotmail.com>
2013-07-28 22:08:09 +0200

    Added support for tdAddDevice, tdRemoveDevice and tdGetErrorString
    tdAddDevice just adds an empty device with no name, protocol, device code, etc. Will add support for:
    -tdSetName
    -tdSetModel
    -tdSetProtocol
    -tdSetDeviceParameter
    -tdLearn

--- telldus.js
+++ telldus.js
@@ -2,6 +2,8 @@ var tellduscore = require('./build/Release/telldus-core-js');
 
 (function (exports, global) {
   exports.getDevices = function() { return tellduscore.getDevices(); };
+  exports.addDevice = function() { return tellduscore.addDevice(); };
+  exports.removeDevice = function(id) { return tellduscore.removeDevice(id); };
   exports.turnOn = function(id) { return tellduscore.turnOn(id); };
   exports.turnOff = function(id) { return tellduscore.turnOff(id); };
   exports.dim = function(id, levl) { return tellduscore.dim(id, levl); };
@@ -9,4 +11,5 @@ var tellduscore = require('./build/Release/telldus-core-js');
   exports.addSensorEventListener = function(callback) { return tellduscore.addSensorEventListener(callback); };
   exports.addRawDeviceEventListener = function(callback) { return tellduscore.addRawDeviceEventListener(callback); };
   exports.removeEventListener = function(id) { return tellduscore.removeEventListener(id); };
+  exports.getErrorString = function(id) { return tellduscore.getErrorString(id); };
 })('object' === typeof module ? module.exports : (this.telldus = {}), this);
