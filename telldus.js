9c48ea475bd9355b59d8c603f78008796a210482
Robin Nilsson <robinnilsson@gmail.com>
2013-11-21 22:32:56 +0100

    Adapting to 0.10

--- telldus.js
+++ telldus.js
@@ -1,12 +1,12 @@
 var telldus = require('./build/Release/telldus');
 
 (function (exports, global) {
-  exports.getDevices = function() { return tellduscore.getDevices(); };
-  exports.turnOn = function(id) { return tellduscore.turnOn(id); };
-  exports.turnOff = function(id) { return tellduscore.turnOff(id); };
-  exports.dim = function(id, levl) { return tellduscore.dim(id, levl); };
-  exports.addDeviceEventListener = function(callback) { return tellduscore.addDeviceEventListener(callback); };
-  exports.addSensorEventListener = function(callback) { return tellduscore.addSensorEventListener(callback); };
-  exports.addRawDeviceEventListener = function(callback) { return tellduscore.addRawDeviceEventListener(callback); };
-  exports.removeEventListener = function(id) { return tellduscore.removeEventListener(id); };
+  exports.getDevices = function() { return telldus.getDevices(); };
+  exports.turnOn = function(id) { return telldus.turnOn(id); };
+  exports.turnOff = function(id) { return telldus.turnOff(id); };
+  exports.dim = function(id, levl) { return telldus.dim(id, levl); };
+  exports.addDeviceEventListener = function(callback) { return telldus.addDeviceEventListener(callback); };
+  exports.addSensorEventListener = function(callback) { return telldus.addSensorEventListener(callback); };
+  exports.addRawDeviceEventListener = function(callback) { return telldus.addRawDeviceEventListener(callback); };
+  exports.removeEventListener = function(id) { return telldus.removeEventListener(id); };
 })('object' === typeof module ? module.exports : (this.telldus = {}), this);
